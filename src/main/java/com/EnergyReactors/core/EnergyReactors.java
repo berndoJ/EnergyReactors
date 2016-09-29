3303f8270b3249988ca0d20c81e2bde8375e2280
jojo1230 <johannesberndorfer82@gmail.com>
2016-09-29 18:19:05 +0200

    Changed Main Class and deleted Example Mod Files

--- src/main/java/com/EnergyReactors/core/EnergyReactors.java
+++ src/main/java/com/EnergyReactors/core/EnergyReactors.java
@@ -1,5 +1,40 @@
 package com.EnergyReactors.core;
 
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Main Class of EnergyReactors Mod
+ * @author Johannes
+ *
+ */
+@Mod(modid = EnergyReactors.MODID, version = EnergyReactors.VERSION)
 public class EnergyReactors {
+	
+	//Mod-Constants
+    public static final String MODID = "energyreactors";
+    public static final String VERSION = "1.0_1.7.10";
+    
+    //Initialization-Events
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+    	
+    }
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+		
+    }
+    
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+    	
+    }
+    
 
 }
